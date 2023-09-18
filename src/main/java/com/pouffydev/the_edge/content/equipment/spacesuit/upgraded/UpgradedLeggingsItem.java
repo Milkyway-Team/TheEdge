4073d41808c9b846290aa58b784c8df647dc58af
pouffy <pouffygamer@outlook.com>
2023-09-18 22:35:17 +0100

    Directional Dash ability :3

--- src/main/java/com/pouffydev/the_edge/content/equipment/spacesuit/upgraded/UpgradedLeggingsItem.java
+++ src/main/java/com/pouffydev/the_edge/content/equipment/spacesuit/upgraded/UpgradedLeggingsItem.java
@@ -1,9 +1,12 @@
 package com.pouffydev.the_edge.content.equipment.spacesuit.upgraded;
 
 import com.pouffydev.the_edge.TEKeys;
+import com.pouffydev.the_edge.TheEdge;
 import com.pouffydev.the_edge.content.equipment.spacesuit.SpaceArmorItem;
 import com.pouffydev.the_edge.foundation.config.TEConfigs;
+import com.simibubi.create.foundation.utility.Lang;
 import net.minecraft.core.Direction;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
@@ -25,16 +28,19 @@ public class UpgradedLeggingsItem extends SpaceArmorItem {
     public void inventoryTick(@Nonnull ItemStack stack, Level level, @Nonnull Entity entity, int itemSlot, boolean isSelected) {
         super.inventoryTick(stack, level, entity, itemSlot, isSelected);
         Player player = (Player) entity;
-        Vec3 vec3 = entity.getDeltaMovement();
-        Direction dir = entity.getMotionDirection();
-        if (player.isFallFlying()) {
-            entity.setDeltaMovement(vec3.add(dir.getStepX() * boost, dir.getStepY() * boost, dir.getStepZ() * boost));
+        if (TEKeys.boost.isDown() && !entity.isSteppingCarefully() && !entity.isOnGround() && !player.isFallFlying() && !((Player) entity).getCooldowns().isOnCooldown(this)) {
+            //entity.setDeltaMovement(vec3.x, vec3.y + boost, vec3.z);
+            boostInLookDirection(entity);
+            ((Player) entity).getCooldowns().addCooldown(this, 999999999);
         }
-        if (TEKeys.boost.isDown() && !entity.isSteppingCarefully() && !player.isFallFlying() && !((Player) entity).getCooldowns().isOnCooldown(this)) {
-            entity.setDeltaMovement(vec3.x, vec3.y + boost, vec3.z);
-            ((Player) entity).getCooldowns().addCooldown(this, 300);
-        }
-        if (entity.fallDistance == 0)
+        if (((Player) entity).getCooldowns().isOnCooldown(this))
+            player.sendMessage((Component) Lang.translate(TheEdge.ID, "message.boost_cooldown"), player.getUUID());
+        if (entity.isOnGround())
             ((Player) entity).getCooldowns().removeCooldown(this);
+    }
+    private void boostInLookDirection(Entity entity) {
+        Vec3 vec3 = entity.getDeltaMovement();
+        Vec3 dir = entity.getLookAngle();
+        entity.setDeltaMovement(vec3.add(dir.get(Direction.Axis.X) * boost, dir.get(Direction.Axis.Y) * boost, dir.get(Direction.Axis.Z) * boost));
     }
 }
